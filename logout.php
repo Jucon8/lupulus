2965d779e4a220d4f7d13e32326ccd50b42d6ee7
Santi-Bobadilla <58020402+Santi-Bobadilla@users.noreply.github.com>
2019-12-29 05:07:56 -0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -6,6 +6,6 @@ session_destroy();
 // setcookie("usuarioEmail", null, time()- 1); //
 // setcookie("usuarioPassword", null, time()- 1); //
 
-header("Location: iniciarsesion.php")
+header("Location: index.php")
 
  ?>
