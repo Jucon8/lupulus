9b7c402b8fb1e509a97a55aab948e4dc85964a72
Juan José Condinanzi <jucon8@gmail.com>
2020-01-18 23:55:12 -0300

    panel admin + estilos

---
+++ admin.php
@@ -0,0 +1,314 @@
+<?php
+
+session_start();
+$titulo="Panel de Control";
+?>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+
+<head>
+  <?php require_once 'head.php' ?>
+</head>
+
+  <body>
+  <?php require_once 'header.php' ?>
+    <div class="container-fluid" id="bodyAdmin">
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block  sidebar">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  <span data-feather="home"></span>
+                  Panel de control <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file"></span>
+                  Pedidos
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="shopping-cart"></span>
+                  Productos
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="users"></span>
+                  Clientes
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="bar-chart-2"></span>
+                  Estadisticas
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="layers"></span>
+                  Integraciones
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+            <h1 class="h2">Panel de Control</h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
+              <div class="btn-group mr-2">
+                <button class="btn btn-sm btn-outline-secondary">Compartir</button>
+                <button class="btn btn-sm btn-outline-secondary">Exportar</button>
+              </div>
+              <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
+                <span data-feather="calendar"></span>
+                Esta semana
+              </button>
+            </div>
+          </div>
+          <div class="panel-body">
+                    <div id="chart">
+            <canvas class="my-4" id="myChart" width="2" height="1"></canvas>
+            </div>
+                </div>
+
+          <h2>Productos publicados</h2>
+          <div class="table-responsive">
+            <table class="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th>Id_Producto</th>
+                  <th>Nombre</th>
+                  <th>Descripcion</th>
+                  <th>Precio</th>
+                  <th>Stock</th>
+                  <th>Estado</th>
+                  <th>Imagenes</th>
+                  <th>Id_Categoria</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>1</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>0</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>15</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>5</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>5</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>1</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>6</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>0</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>7</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>1</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>8</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>100</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>9</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>1000</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>10</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>23</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>11</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>14</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>12</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>15</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>13</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>52</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>14</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>55</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>15</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>23</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+                <tr>
+                  <td>16</td>
+                  <td>Producto</td>
+                  <td>Lorem ipsum dolor sit amet.</td>
+                  <td>$$$$$$$</td>
+                  <td>14</td>
+                  <td>Activo</td>
+                  <td>imagen1, imagen2, imagen3</td>
+                  <td>Categoria #</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </main>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <!-- Icons -->
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+    <script>
+      feather.replace()
+    </script>
+
+    <!-- Graphs -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+    <script>
+      var ctx = document.getElementById("myChart");
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"],
+          datasets: [{
+            data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
+            lineTension: 0.2,
+            backgroundColor: 'transparent',
+            borderColor: '#f0b500',
+            borderWidth: 4,
+            pointBackgroundColor: '#f0b500'
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: false
+              }
+            }]
+          },
+          legend: {
+            display: false,
+          }
+        }
+      });
+    </script>
+        <footer>
+        <?php require_once 'footer.php' ?>
+    </footer>
+  </body>
+</html>
