1ecc8d15d48c49c8f18ba84b0a270abe3d946571
Juan José Condinanzi <jucon8@gmail.com>
2020-01-27 17:25:38 -0300

    centrado header menu

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul class="navbar-nav mr-auto ">
+        <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
                 <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
             </li>
