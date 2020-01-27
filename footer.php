a47159909c40a77adc1831478f24761dc43ed9e4
Juan José Condinanzi <jucon8@gmail.com>
2020-01-27 17:25:24 -0300

    cambio iconos + enlace al repo

--- footer.php
+++ footer.php
@@ -1,17 +1,17 @@
 <footer id="footer">
     <div class="flex-container">
-        <div> <a class="btn-floating  accent2" href="https://facebook.com"><i class="icono icon ion-logo-facebook"></i></a></div>
-        <div> <a class="btn-floating  accent2" href="https://instagram.com"><i class="icono icon ion-logo-instagram"></i></a></div>
+        <div> <a class="btn-floating  accent2" href="https://facebook.com"><span data-feather="facebook" id="iconos-footer"></span></a></div>
+        <div> <a class="btn-floating  accent2" href="https://instagram.com"><span data-feather="instagram" id="iconos-footer"></span></a></div>
         <div>  <a href="index.php"> <img src="img/lupulo.svg" class="img-top" alt="logo"></a> </div>
-        <div> <a class="btn-floating  accent2" href="https://youtube.com"><i id="youtube" class="icono icon ion-logo-youtube"></i></a>
+        <div> <a class="btn-floating  accent2" href="https://youtube.com"><span data-feather="youtube" id="iconos-footer"></span></a>
         </div>
-        <div> <a class="btn-floating  accent2" href="https://twitter.com"><i class="icono icon ion-logo-twitter"></i></a>
+        <div> <a class="btn-floating  accent2" href="https://twitter.com"><span data-feather="twitter" id="iconos-footer"></span></a>
         </div>
 
     </div>
     <div>
-        <div class="card-footer text-center bg-warning">
-            <h6> <strong> Desarrollado por 5Bits </strong> </h6>
+        <div class="card-footer text-center bg-warning" id="footericonos">
+            <h5> <strong> Desarrollado por 5Bits <a href="https://github.com/Jucon8/lupulus" id="git"><span data-feather="github" class="feather"></span></strong></a></h5>
         </div>
 
     </div>
