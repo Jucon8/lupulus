fc357687dfa2f2c58e6590513916225a36c8d14c
Juan José Condinanzi <jucon8@gmail.com>
2020-02-12 15:20:42 -0300

    Arreglamos header y home

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+#Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('/admin/user', 'UserAdminController');
 
