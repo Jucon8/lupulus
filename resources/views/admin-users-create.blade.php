5a1d1107e737a032f9109ffad72cfc515f527b4a
Juan José Condinanzi <jucon8@gmail.com>
2020-02-12 21:01:14 -0300

    Vista de creacion de nuevos Usuarios en ADMIN panel

--- resources/views/admin-users-create.blade.php
+++ resources/views/admin-users-create.blade.php
@@ -3,66 +3,56 @@
 
 @section('contenido')
 
-<br>
-<br>
-<br>
-<br>
-<br>
 
-{!! Form::open() !!}
-				<form class="container col-md-9 col-lg-12" id="register-form" action="users.store" method="POST" action="{{ route('register') }}" role="form" enctype="multipart/form-data">
-                @csrf
-
+<br>
+<br>
+<div class="container-fluid bg-light">
+    <div class="row">
+    <div class="col-lg-12 bg-light text-center">
+    <h1>Crear Usuarios</h1>
+    </div>
+    <div class="col-lg-12 bg-light align-content-center">
+{!! Form::open(['route'=>'users.store', 'method'=>'POST']) !!}
+                {!! Form::token() !!}
                     <div class="form-group">
-                        <label hidden for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
-                        <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus placeholder="Nombre de usuario">
+                        {!! Form::label('username', 'Nombre') !!}
+                        {!! Form::text('username', null, ['class'=>'form-control', 'placeholder'=>'Nombre', 'required']) !!}
                         @error('username')
                             <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
+                               <strong>{{ $message }}</strong>
                             </span>
-                        @enderror
-                    </div>
-
-					<div class="form-group">
-                        <label hidden for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Correo electrónico">
-
-                        @error('email')
+                      @enderror
+                  </div>
+                    <div class="form-group">
+                        {!! Form::label('email', 'Correo Electrónico') !!}
+                        {!! Form::email('email', null, ['class'=>'form-control', 'placeholder'=>'Example@gmail.com', 'required']) !!}
+                        @error('username')
                             <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
+                               <strong>{{ $message }}</strong>
                             </span>
-                        @enderror
-					</div>
-
-					<div class="form-group">
-						<label for="avatar">Subí tu avatar (Opcional)</label>
-						<input type="file" class="form-control-file" name="avatar" enctype="multipart/form-data" accept= "image/*">
-                        @error('avatar')
+                      @enderror
+                  </div>
+                    <div class="form-group">
+                        {!! Form::label('password', 'Contraseña') !!}
+                        {!! Form::password('password', ['class'=>'form-control', 'placeholder'=>'********', 'required']) !!}
+                        @error('username')
                             <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
+                               <strong>{{ $message }}</strong>
                             </span>
-                        @enderror
-					</div>
+                      @enderror
+                  </div>
+                  <div class="form-group">
+                    {!! Form::label('rol_id', 'Rol') !!}
+                    {!! Form::select('rol_id', ['null'=>'Seleccione una opción', '1' => 'Administrador', '2' => 'Cliente', '3' => 'Editor', '4'=> 'Proveedor', '5'=>'Distribuidor', '6'=>'Vendedor'], 'null', ['class' => 'form-control']) !!}
+                  </div>
 
-					<div class="form-group">
-                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Contraseña">
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-					</div>
-
-					<div class="form-group">
-                    <label hidden for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirmar contraseña">
-					</div>
-
-					<div class="text-center w-50 border border-secondary rounded" id="formulario">
-						<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn-warning" value="{{ __('Crear cuenta') }}">
-					</div>
-				</form>
+                  <div class="form-group align-content-center">
+                    {!! Form::submit('Crear Usuario', ['class'=>'btn btn-warning col-12']) !!}
+                  </div>
 
 
+        {!! Form::close() !!}
+        </div>
+    </div>
+</div>
 @endsection
